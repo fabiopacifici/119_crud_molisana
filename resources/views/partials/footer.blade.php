e9a06d124c3b66eaf9c35c98d53eb27843eb5aaf
fbhood <mail@fabiopacifici.com>
2024-05-15 12:25:07 +0200

    Add header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,14 @@
-<footer>Footer</footer>
+<footer class="bg-dark text-white-50 py-5">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p>
+                    <i class="fas fa-copyright fa-sm fa-fw"></i>
+                    Boolean
+                </p>
+            </div>
+        </div>
+    </div>
+
+</footer>
