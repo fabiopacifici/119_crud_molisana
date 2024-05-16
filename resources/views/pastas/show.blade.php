de330669ffaface431786eae4f1ea6ccf9b2b328
fbhood <mail@fabiopacifici.com>
2024-05-16 10:57:11 +0200

    Add links to the show view
    Added links to edit and go back to all pastas items

--- resources/views/pastas/show.blade.php
+++ resources/views/pastas/show.blade.php
@@ -9,14 +9,26 @@
             <img src="{{$pasta->src}}" alt="">
         </div>
         <div class="col">
-            <h2>{{$pasta->title}}</h2>
+
+            <div class="d-flex justify-content-between">
+                <h2>{{$pasta->title}}</h2>
+                <div class="actions">
+                    <a class="btn btn-dark" href="{{route('pastas.edit', $pasta)}}">
+                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                    </a>
+                    <a class="btn btn-secondary" href="{{route('pastas.index')}}">
+                        <i class="fas fa-arrow-left fa-sm fa-fw"></i>
+                        Back to patas
+                    </a>
+                </div>
+            </div>
+
             <p>{{$pasta->description}}</p>
             <hr>
             <div class="metadata">
                 <strong>Type:</strong> {{$pasta->type}} <br>
                 <strong>Weight:</strong> {{$pasta->weight}} <br>
                 <strong>Cooking time:</strong> {{$pasta->cooking_time}} <br>
-
             </div>
         </div>
     </div>
